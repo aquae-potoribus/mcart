bfcf18a217b7bde54fb0415aef32d5f85c4f383c
sergei_kondrashkin <sergeikondrashkins@gmail.com>
2023-09-09 01:50:39 +0300

    task3.7

--- seller-personal-account/my-ads/index.php
+++ seller-personal-account/my-ads/index.php
@@ -1,7 +1,11 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Мои объявления");
-?><?$APPLICATION->IncludeComponent(
+?><?
+$arrFilter['CREATED_BY'] = $GLOBALS['USER']->GetID();;
+$GLOBALS['arrFilter'] = $arrFilter;
+?>
+<?$APPLICATION->IncludeComponent(
 	"bitrix:news",
 	"ads_templ",
 	Array(
@@ -18,15 +22,14 @@ $APPLICATION->SetTitle("Мои объявления");
 		"CACHE_TIME" => "36000000",
 		"CACHE_TYPE" => "A",
 		"CHECK_DATES" => "Y",
-		"COMPONENT_TEMPLATE" => "ads_templ",
 		"DETAIL_ACTIVE_DATE_FORMAT" => "",
 		"DETAIL_DISPLAY_BOTTOM_PAGER" => "Y",
 		"DETAIL_DISPLAY_TOP_PAGER" => "N",
-		"DETAIL_FIELD_CODE" => array(0=>"NAME",1=>"DETAIL_TEXT",2=>"",),
+		"DETAIL_FIELD_CODE" => array("NAME","DETAIL_TEXT",""),
 		"DETAIL_PAGER_SHOW_ALL" => "Y",
 		"DETAIL_PAGER_TEMPLATE" => "",
 		"DETAIL_PAGER_TITLE" => "Страница",
-		"DETAIL_PROPERTY_CODE" => array(0=>"LOCATION",1=>"NUMB_BATHROOMS",2=>"NUMB_FLOORS",3=>"GARAGE",4=>"SQUARE",5=>"LINKS_EXTERNAL_MATERIALS",6=>"PRICE",7=>"",8=>"",),
+		"DETAIL_PROPERTY_CODE" => array("LOCATION","NUMB_BATHROOMS","NUMB_FLOORS","GARAGE","SQUARE","LINKS_EXTERNAL_MATERIALS","PRICE",""),
 		"DETAIL_SET_CANONICAL_URL" => "N",
 		"DISPLAY_BOTTOM_PAGER" => "Y",
 		"DISPLAY_DATE" => "Y",
@@ -34,13 +37,16 @@ $APPLICATION->SetTitle("Мои объявления");
 		"DISPLAY_PICTURE" => "Y",
 		"DISPLAY_PREVIEW_TEXT" => "Y",
 		"DISPLAY_TOP_PAGER" => "N",
+		"FILTER_FIELD_CODE" => array("CREATED_BY",""),
+		"FILTER_NAME" => "arrFilter",
+		"FILTER_PROPERTY_CODE" => array("",""),
 		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
 		"IBLOCK_ID" => "5",
 		"IBLOCK_TYPE" => "ads",
 		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
 		"LIST_ACTIVE_DATE_FORMAT" => "",
-		"LIST_FIELD_CODE" => array(0=>"",1=>"PROPERTY_PRICE",2=>"PROPERTY_NUMB_FLOORS",3=>"PROPERTY_SQUARE",4=>"PROPERTY_NUMB_BATHROOMS",5=>"PROPERTY_GARAGE",6=>"PROPERTY_LOCATION",7=>"",),
-		"LIST_PROPERTY_CODE" => array(0=>"",1=>"",),
+		"LIST_FIELD_CODE" => array("","PROPERTY_PRICE","PROPERTY_NUMB_FLOORS","PROPERTY_SQUARE","PROPERTY_NUMB_BATHROOMS","PROPERTY_GARAGE","PROPERTY_LOCATION",""),
+		"LIST_PROPERTY_CODE" => array("",""),
 		"MESSAGE_404" => "",
 		"META_DESCRIPTION" => "-",
 		"META_KEYWORDS" => "-",
@@ -55,7 +61,7 @@ $APPLICATION->SetTitle("Мои объявления");
 		"PREVIEW_TRUNCATE_LEN" => "",
 		"SEF_FOLDER" => "/ads/",
 		"SEF_MODE" => "Y",
-		"SEF_URL_TEMPLATES" => array("news"=>"","section"=>"","detail"=>"#ELEMENT_CODE#/",),
+		"SEF_URL_TEMPLATES" => Array("detail"=>"#ELEMENT_CODE#/","news"=>"","section"=>""),
 		"SET_LAST_MODIFIED" => "N",
 		"SET_STATUS_404" => "N",
 		"SET_TITLE" => "Y",
@@ -66,7 +72,7 @@ $APPLICATION->SetTitle("Мои объявления");
 		"SORT_ORDER2" => "ASC",
 		"STRICT_SECTION_CHECK" => "N",
 		"USE_CATEGORIES" => "N",
-		"USE_FILTER" => "N",
+		"USE_FILTER" => "Y",
 		"USE_PERMISSIONS" => "N",
 		"USE_RATING" => "N",
 		"USE_REVIEW" => "N",
